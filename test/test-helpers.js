528b48040f08e2d13d9e4837f718cd8897fec175
William Diffey <50226488+williamdiffey@users.noreply.github.com>
2020-11-27 23:09:41 +0900

    added more tests for user endpoint

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,7 +1,19 @@
-function cleanTables(db) {
-  return db.raw(`TRUNCATE users`)
+function cleanUserTable(db) {
+  // drop rather than truncate to avoid primary key increments breaking
+  // expected values when returning a new user
+
+  return db.raw(`DROP TABLE IF EXISTS users;
+  CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    avatar_url TEXT NOT NULL DEFAULT 'Default-Avatar.png',
+    not_verified BOOLEAN DEFAULT true
+);
+  `)
 }
 
 module.exports = {
-  cleanTables,
+  cleanUserTable,
 }
--- test/user-endpoints.spec.js
+++ test/user-endpoints.spec.js
@@ -14,8 +14,8 @@ describe('User Endpoints', () => {
   })
 
   after('disconnect form db', () => db.destroy())
-  before('cleanup', () => helpers.cleanTables(db))
-  afterEach('cleanup', () => helpers.cleanTables(db))
+  before('cleanup', () => helpers.cleanUserTable(db))
+  afterEach('cleanup', () => helpers.cleanUserTable(db))
 
   describe('POST user', () => {
     it('responds with an error given an invalid user post', () => {
@@ -24,6 +24,53 @@ describe('User Endpoints', () => {
         password: 'PASSWORD1!',
       }
       return supertest(app).post('/api/user/').send(user).expect(400)
+    })
+    it('responds with 201 and the json data given a valid request', () => {
+      const user = {
+        id: 1,
+        email: 'test@test.com',
+        username: 'test',
+        password: 'Password1!',
+      }
+
+      const expected = {
+        id: 1,
+        email: 'test@test.com',
+        username: 'test',
+      }
+      return supertest(app)
+        .post('/api/user')
+        .send(user)
+        .expect(201, expected)
+        .expect('Content-Type', /json/)
+    })
+  })
+
+  describe('GET user', () => {
+    context('given there is a user in the db', () => {
+      beforeEach('insert seed data', () => {
+        const user = [
+          {
+            username: 'test',
+            avatar_url: 'https://i.imgur.com/eqaKDBlt.jpg',
+            email: 'test@test.com',
+            password:
+              '$2y$12$R6sg9E77/5c41GJZIGqlH.UH73UPmxFwa6S6U1DMvA4MjSgIyGtVG',
+          },
+        ]
+        return db.into('users').insert(user)
+      })
+      it('responds with the user', () => {
+        const expected = {
+          userInfo: {
+            id: 1,
+            username: 'test',
+            avatar_url: 'https://i.imgur.com/eqaKDBlt.jpg',
+            email: 'test@test.com',
+          },
+        }
+        return supertest(app).get('/api/user/1').expect(200, expected)
+      })
     })
   })
 })
